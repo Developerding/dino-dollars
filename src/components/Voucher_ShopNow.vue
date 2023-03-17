dacf39848579065b0b641188c49149cb0c92d542
kohchloe <chloekoh.2021@scis.smu.edu.sg>
2023-03-17 15:55:38 +0800

    test

--- src/components/Voucher_ShopNow.vue
+++ src/components/Voucher_ShopNow.vue
@@ -1,5 +1,49 @@
 <template>
+
+
+
+<v-col cols="12">
+          <v-card
+            color="#1F7087"
+            theme="dark"
+          >
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title class="text-h5">
+                  Supermodel
+                </v-card-title>
+
+                <v-card-subtitle>Foster the People</v-card-subtitle>
+
+                <v-card-actions>
+                  <v-btn
+                    class="ms-2"
+                    variant="outlined"
+                    size="small"
+                  >
+                    START RADIO
+                  </v-btn>
+                </v-card-actions>
+              </div>
+
+              <v-avatar
+                class="ma-3"
+                size="125"
+                rounded="0"
+              >
+                <v-img src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"></v-img>
+              </v-avatar>
+            </div>
+          </v-card>
+        </v-col>
+
+
+
+
+
+
     <v-card class="mx-auto rounded-lg" outlined>
+
         <v-list-item three-line>
         <v-list-item-content>
             <v-list-item-title class="text-h5 mb-1" >$5 OFF</v-list-item-title>
