ee813e92eae8d7f78bafb092063b29d4c6ba5b79
kohchloe <chloekoh.2021@scis.smu.edu.sg>
2023-04-01 21:51:41 +0800

    more buyVoucher.py

--- src/backend/buyVoucher.py
+++ src/backend/buyVoucher.py
@@ -18,11 +18,25 @@ def buy_voucher(UID, Cost):
     
     if request.is_json:
         try:
+            order = request.get_json()
+            print("\nReceived an order in JSON:", order)
+
+            # do the actual work
+            # 1. Send order info {cart items}
             newVoucher = createNewVoucher()
-            newBalance = currentBalance(UID) - Cost
-            updateNewBalance = updateUserBalance(UID) 
-            
-            
+            newBalance = getCurrentBalance(UID) - Cost
+
+            result = updateUserBalance(order, UID)
+            print('\n------------------------')
+            print('\nresult: ', result)
+            return jsonify(result), result["code"]
+
+            # newVoucher = createNewVoucher()
+            # newBalance = getCurrentBalance(UID) - Cost
+            # updateNewBalance = updateUserBalance(UID) 
+            # # purchasedVoucher = returnPurchasedVoucher(UID)
+            # return newVoucher            
+
         except Exception as e:
             # Unexpected error in code
             exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -46,20 +60,82 @@ def createNewVoucher():
     newVoucher = invoke_http(url, method='POST')
     return newVoucher
 
-def currentBalance(UID):
+def getCurrentBalance(UID):
     url = "http://localhost:5003/user/" + str(UID)
     user = invoke_http(url, method='GET')
     return user['data']['Points']
 
-def updateUserBalance(UID):
-    url = "http://localhost:5003/point/" + str(UID)
-    updatedBalance = invoke_http(url, method='PUT')
-    return updatedBalance
 
-def returnPurchasedVoucher(UID):
-    url = "http://localhost:5002/purchasedvoucher/" + str(UID) 
-    purchasedVoucher = invoke_http(url, method='GET')
-    return purchasedVoucher
+def updateUserBalance(order, UID):
+    # 2. Send the order info {cart items}
+    # Invoke the order microservice
+    print('\n-----Invoking order microservice-----')
+    print(order)
+    user_url = "http://localhost:5003/point/" + str(UID)
+    print(user_url)
+    order_result = invoke_http(user_url, method='PUT', json=order)
+    print('order_result:', order_result)
+
+    # Check the order result; if a failure, send it to the error microservice.
+    code = order_result["code"]
+    message = json.dumps(order_result)
+
+    if code not in range(200, 300):
+        # Inform the error microservice
+        #print('\n\n-----Invoking error microservice as order fails-----')
+        print('\n\n-----Publishing the (order error) message with routing_key=order.error-----')
+
+        # invoke_http(error_URL, method="POST", json=order_result)
+        amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="order.error", 
+            body=message, properties=pika.BasicProperties(delivery_mode = 2)) 
+        # make message persistent within the matching queues until it is received by some receiver 
+        # (the matching queues have to exist and be durable and bound to the exchange)
+
+        # - reply from the invocation is not used;
+        # continue even if this invocation fails        
+        print("\nOrder status ({:d}) published to the RabbitMQ Exchange:".format(
+            code), order_result)
+
+        # 7. Return error
+        return {
+            "code": 500,
+            "data": {"order_result": order_result},
+            "message": "Failure to update dino dollars balance sent for error handling."
+        }
+
+    # Notice that we are publishing to "Activity Log" only when there is no error in order creation.
+    # In http version, we first invoked "Activity Log" and then checked for error.
+    # Since the "Activity Log" binds to the queue using '#' => any routing_key would be matched 
+    # and a message sent to “Error” queue can be received by “Activity Log” too.
+
+    # else:
+    #     # 4. Record new order
+    #     # record the activity log anyway
+    #     #print('\n\n-----Invoking activity_log microservice-----')
+    #     print('\n\n-----Publishing the (order info) message with routing_key=order.info-----')        
+
+    #     # invoke_http(activity_log_URL, method="POST", json=order_result)            
+    #     amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="order.info", 
+    #         body=message)
+    
+    print("\nOrder published to RabbitMQ Exchange.\n")
+    return {
+            "code": 200,
+            "currentPoints": order_result["data"]["Points"],
+            }    # - reply from the invocation is not used;
+    # continue even if this invocation fails
+    
+
+# oversimplified one
+# def updateUserBalance(UID):
+#     url = "http://localhost:5003/point/" + str(UID)
+#     updatedBalance = invoke_http(url, method='PUT')
+#     return updatedBalance
+
+# def returnPurchasedVoucher(UID):
+#     url = "http://localhost:5002/purchasedvoucher/" + str(UID) 
+#     purchasedVoucher = invoke_http(url, method='GET')
+#     return purchasedVoucher
 
 
 if __name__ == "__main__":
